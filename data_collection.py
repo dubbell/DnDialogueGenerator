5c35f0116bc647362b581c59bcfc5a1ca43aaebf
Linus Lundgren <linus.c.lundgren@gmail.com>
2024-10-14 18:42:40 +0200

    Optimize data collection and start implementing training loop.

--- data_collection.py
+++ data_collection.py
@@ -1,54 +1,48 @@
 from datasets import load_dataset
 from torch.utils.data import Dataset
+import torch
 
 
 class DialogueDataset(Dataset):
-    def __init__(self, datasets, dataset_labels):
-        self.datasets = datasets
-        self.dataset_labels = dataset_labels
+    def __init__(self, datasets, dataset_labels, tokenizer):
+        self.data = []
+        for dataset, dataset_label in zip(datasets, dataset_labels):
+            for item in dataset:                
+                categories = ','.join(item["categories"]) + "," + dataset_label
+                personalities = ','.join(item["personalities"])
+                conversations = '\n'.join([line["content"] for line in item["conversation"]])
+
+                tokens = tokenizer.encode(categories + "<|endoftext|>" + personalities + "<|endoftext|>" + conversations)
+
+                self.data.append(tokens)
+        
+        longest = max([len(seq) for seq in self.data])
+        for index in range(len(self.data)):
+            self.data[index].extend([tokenizer.pad_token] * (longest - len(self.data[index])))
+            self.data[index] = torch.tensor(self.data[index])
+
 
     def __len__(self):
-        return sum(len(dataset) for dataset in self.datasets)
+        return len(self.data)
     
     def __getitem__(self, idx):
-        count = 0
-        for dataset, dataset_label in zip(self.datasets, self.dataset_labels):
-            if count + len(dataset) >= idx:
-                count += len(dataset)
-                continue
-            
-            item = dataset[idx - count].copy()
-            item["type"] = dataset_label
-            return item
+        return self.data[idx]
     
 
-def train_val_test(dataset_names, train_size = 0.8, val_size = 0.1, test_size = 0.1):
-    assert train_size + val_size + test_size == 1, "train_size, val_size and test_size must add up to 1"
-    train_sets, val_sets, test_sets = [], [], []
+def training_set(dataset_names, tokenizer):
+    train_sets = []
     dataset_labels = []
 
     if "romantic" in dataset_names:
-        train_valtest = load_dataset("AlekseyKorshuk/synthetic-romantic-characters")["train"].train_test_split(test_size=val_size + test_size)
-        val_test = train_valtest["test"].train_test_split(test_size = test_size / (test_size + val_size))
-        train_sets.append(train_valtest["train"])
-        val_sets.append(val_test["train"])
-        test_sets.append(val_test["test"])
+        train_sets.append(load_dataset("AlekseyKorshuk/synthetic-romantic-characters")["train"])
         dataset_labels.append("romantic")
-    
+
     if "friendly" in dataset_names:
-        train_valtest = load_dataset("AlekseyKorshuk/synthetic-friendly-characters")["train"].train_test_split(test_size=val_size + test_size)
-        val_test = train_valtest["test"].train_test_split(test_size = test_size / (test_size + val_size))
-        train_sets.append(train_valtest["train"])
-        val_sets.append(val_test["train"])
-        test_sets.append(val_test["test"])
-        dataset_labels.append("romantic")
-    
+        train_sets.append(load_dataset("AlekseyKorshuk/synthetic-friendly-characters")["train"])
+        dataset_labels.append("friendly")
+
     if "fight" in dataset_names:
-        train_valtest = load_dataset("AlekseyKorshuk/synthetic-fight-characters")["train"].train_test_split(test_size=val_size + test_size)
-        val_test = train_valtest["test"].train_test_split(test_size = test_size / (test_size + val_size))
-        train_sets.append(train_valtest["train"])
-        val_sets.append(val_test["train"])
-        test_sets.append(val_test["test"])
-        dataset_labels.append("romantic")
+        train_sets.append(load_dataset("AlekseyKorshuk/synthetic-fight-characters")["train"])
+        dataset_labels.append("fight")
     
-    return DialogueDataset(train_sets, dataset_labels), DialogueDataset(val_sets, dataset_labels), DialogueDataset(test_sets, dataset_labels)
+    return DialogueDataset(train_sets, dataset_labels, tokenizer)
