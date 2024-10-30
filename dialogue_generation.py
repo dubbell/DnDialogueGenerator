386687dc74d1475284415978ec546e74dee167a5
Linus Lundgren <linus.c.lundgren@gmail.com>
2024-10-30 13:10:20 +0100

    Final stuff.

--- dialogue_generation.py
+++ dialogue_generation.py
@@ -1,6 +1,6 @@
 from dialogue_generator import DialogueGenerator
 
 
-dialogue_generator = DialogueGenerator()
+dialogue_generator = DialogueGenerator("dialogue_model4.p")
 
-dialogue_generator.generate_dialogue(.7)
+dialogue_generator.generate_dialogue(.6)
--- dialogue_generator.py
+++ dialogue_generator.py
@@ -1,16 +1,15 @@
 import torch
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, StoppingCriteria
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, StoppingCriteria, GenerationConfig
 import logging
 import pickle
+import torch.nn as nn
 
 
 class DialogueGenerator:
-    def __init__(self):
+    def __init__(self, model_name):
         logging.getLogger('transformers').setLevel(logging.ERROR)
 
-        self.dialogue_model = GPT2LMHeadModel.from_pretrained("gpt2")
-        pretrained_state_dict = pickle.load(open("dialogue_model.p", "rb"))["state_dict"]
-        self.dialogue_model.load_state_dict(pretrained_state_dict)
+        self.dialogue_model = pickle.load(open(model_name, "rb"))["model"]
 
         self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2", clean_up_tokenization_spaces = False)
 
@@ -20,13 +19,38 @@ class DialogueGenerator:
         self.sep_token2 = self.tokenizer.encode("<|endoftext|>")[0]
         self.sep_token3 = self.tokenizer.encode("\n")[0]
 
+        self.generation_config = GenerationConfig(repetition_penalty = 1.5, 
+                                                  no_repeat_ngram_size = 5, 
+                                                  length_penalty = 1.5,
+                                                  num_beams = 5,
+                                                  stop_strings = ["\n"])
+
 
     def generate_dialogue(self, temp):
         model_input, input_length = self._get_model_input()
 
+        dialogue_line = 0
+
         while True:
             tokens = self._generate_tokens(model_input, temp)
-            npc_response = tokens[:, input_length:]
+
+            newline_indices = (tokens[0] == self.sep_token3).nonzero().squeeze()
+            
+            if self.sep_token3 not in tokens[0]:
+                next_line_end = -1
+            elif newline_indices.dim() == 0:
+                next_line_end = newline_indices
+                dialogue_line += 1
+            else:
+                next_line_end = newline_indices[dialogue_line]
+                dialogue_line += 1
+
+            npc_response = tokens[:, input_length:next_line_end]
+            tokens = tokens[:, :next_line_end]
+
+
+            # print("\n" + str(self.tokenizer.batch_decode(tokens.squeeze())) + "\n")
+
             print(f" NPC response: {self.tokenizer.batch_decode(npc_response)[0].strip()}")
 
             response = input("Your response: ")
@@ -42,10 +66,12 @@ class DialogueGenerator:
     def _generate_tokens(self, model_input, temp):
         return self.dialogue_model.generate(input_ids = model_input, 
                                             attention_mask=torch.ones(model_input.shape), 
+                                            generation_config = self.generation_config,
                                             stopping_criteria = self.stopping_criteria, 
                                             temperature=temp, 
                                             do_sample=True,
-                                            max_length = 1024)
+                                            max_length = 1024,
+                                            tokenizer = self.tokenizer)
     
     
     def _get_model_input(self):
